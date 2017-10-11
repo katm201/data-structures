c59102d2162a68f3f84026b23db1056994c8509a
katm201 <katm201@gmail.com>
2017-10-11 15:25:30 -0700

    Implemented the Push and Pop methods in stackMethods (functional shared method pattern)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -12,20 +12,21 @@ var Stack = function() {
 };
 
 var stackMethods = {
+  push: function(value) {
+    this._size++;
+    this._storage[this._size] = value;
+  },
 
-
-
-//push function 
-  //
-//pop function
-
-//size function
+  pop: function() {
+    if (this._size > 0) { 
+      let lastValue = this._storage[this._size];
+      this._size--;
+      return lastValue;
+    }
+  },
   size: function() {
     return this._size;
   }
-
-
-
 };
 
 
