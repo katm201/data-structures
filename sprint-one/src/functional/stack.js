44592769c267673077f9d031ae89e400b086485f
Hack Reactor Student <student@hackreactor.com>
2017-10-11 14:47:10 -0700

    Implemented .size and .push methods for Stack (functional-not shared methods).

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,29 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var _storage = {};
+  var _size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //increase the size
+    _size++;
+    //store the value at storage with the size as the key
+    _storage[_size] = value;
+
   };
 
   someInstance.pop = function() {
+    //check the size
+    //if greater than one then
+      //create a local variable to store the value popping off
+      //reduce size by one
+      //return local variable
+
   };
 
   someInstance.size = function() {
+   return _size;
   };
 
   return someInstance;
