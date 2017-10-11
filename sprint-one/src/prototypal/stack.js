ba62a7a5af5232e452f14ab96d2ec964fd44e244
Hack Reactor Student <student@hackreactor.com>
2017-10-11 15:51:17 -0700

    Instantiated Stack and implemented .size method for our stackMethods object (prototypal pattern).

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function() { 
+  Stack.prototype = stackMethods;
+  var instance = Object.create(Stack.prototype);
+  instance._size = 0;
+  instance._storage = {};
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  
+  //push
+    //increase the size
+    //add the value to the object based on size for the key
+
+  //pop
+    //check to see if size is greater than 0
+      //if it is create a local variable
+      //decrease the size
+      //return the local variable
+
+  size: function() {
+    return this._size;
+  }
+};
 
 
