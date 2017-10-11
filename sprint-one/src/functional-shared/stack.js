23e11308300b2b2adba8a36b11e8632c77305a75
Hack Reactor Student <student@hackreactor.com>
2017-10-11 15:08:59 -0700

    Implemented the size method of the stackMethod object as well as the Stack class instantiation (functional with shared method pattern)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {}; 
+
+  instance._storage = {};
+  instance._size = 0;
+  instance = _.extend(instance, stackMethods);
+
+  return instance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+
+
+//push function 
+  //
+//pop function
+
+//size function
+  size: function() {
+    return this._size;
+  }
+
+
+
+};
 
 
