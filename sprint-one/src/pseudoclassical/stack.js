8d5f09b1f70df3678842ce65b285704b40b7c1ab
katm201 <katm201@gmail.com>
2017-10-11 16:22:05 -0700

    Implemented Stack for pseudoclassical pattern.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,22 +3,19 @@ var Stack = function() {
   this._storage = {};
 };
 
-Stack.prototype = {
-  push: function(value) {
-    this._size++;
-    this._storage[this._size] = value;
-  },
+Stack.prototype.push = function(value) {
+  this._size++;
+  this._storage[this._size] = value;
+};
 
-  pop: function() {
-    if (this._size > 0) {
-      let lastValue = this._storage[this._size];
-      this._size--;
-      return lastValue;
-    }
-  },
-
-  size: function() {
-    return this._size;
+Stack.prototype.pop = function() {
+  if (this._size > 0) {
+    let lastValue = this._storage[this._size];
+    this._size--;
+    return lastValue;
   }
 };
 
+Stack.prototype.size = function() {
+  return this._size;
+};
