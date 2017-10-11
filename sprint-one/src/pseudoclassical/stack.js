932d66bdf183503c4b18931fd8ae6b178b2c7621
Hack Reactor Student <student@hackreactor.com>
2017-10-11 16:11:25 -0700

    Instantiate Stack and and .size method to its .prototype (pseudoclassical pattern).

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,16 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this._size = 0;
+  this._storage = {};
 };
 
+Stack.prototype = {
+  //push
+
+  //pop
+
+  //size
+  size: function() {
+    return this._size;
+  }
+};
 
