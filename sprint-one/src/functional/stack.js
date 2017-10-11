58422f9940b26cf975f03709a6a0a8f469186d06
katm201 <katm201@gmail.com>
2017-10-11 14:53:45 -0700

    Implemented the pop function in the Stack data structure (functional non-shared method pattern)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,24 +7,21 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
-    //increase the size
     _size++;
-    //store the value at storage with the size as the key
     _storage[_size] = value;
-
   };
 
   someInstance.pop = function() {
     //check the size
-    //if greater than one then
-      //create a local variable to store the value popping off
-      //reduce size by one
-      //return local variable
-
+    if (_size > 0) {
+      let lastValue = _storage[_size];
+      _size--;
+      return lastValue;
+    }
   };
 
   someInstance.size = function() {
-   return _size;
+    return _size;
   };
 
   return someInstance;
