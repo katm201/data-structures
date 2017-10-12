b1d300792e639c46cc05644d7dc38a9f53513e7d
Hack Reactor Student <student@hackreactor.com>
2017-10-11 20:18:05 -0700

    Instantiate Queue and implement the .size method on queueMethods object (functional with shared methods pattern).

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+
+  instance = _.extend(instance, queueMethods);
+
+  instance._storage = {};
+
+  instance._size = 0;
+  instance._min = 0;
+  instance._max = 0;
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  //enqueue
+  //dequeue
+  size: function() {
+    debugger;
+    return this._size;
+  }
+};
 
 
