e7bd9697fa9fcbd23dbbf12cbb709804887eee7b
katm201 <katm201@gmail.com>
2017-10-11 20:37:58 -0700

    Implement .dequeue on the queueMethods object (prototypal pattern).

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -3,6 +3,7 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
 
   var instance = Object.create(queueMethods);
+  instance._storage = {};
   instance._size = 0;
   instance._max = 0;
   instance._min = 0;
@@ -10,8 +11,24 @@ var Queue = function() {
 };
 
 var queueMethods = {
-  //enqueue(value)
-  //dequeue
+  enqueue: function(value) {
+    if (this._min === 0) {
+      this._min++;
+    }
+    
+    this._size++;
+    this._max++;
+    this._storage[this._max] = value;
+  },
+
+  dequeue: function() {
+    if (this._size > 0) {
+      let oldestValue = this._storage[this._min];
+      this._min++;
+      this._size--;
+      return oldestValue;
+    }
+  },
 
   size: function() {
     return this._size;
