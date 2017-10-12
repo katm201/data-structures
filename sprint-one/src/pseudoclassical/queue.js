51dfea804e4290cb5f9e9ee0f71376971f9eb0e9
katm201 <katm201@gmail.com>
2017-10-11 20:46:19 -0700

    Instantiated Queue class & added the .size method to the prototype (pseudoclassical pattern).

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,18 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._size = 0;
+  this._min = 0;
+  this._max = 0;
+  this._storage = {};
+
 };
 
 
+Queue.prototype.enqueue = function(value) {};
+
+Queue.prototype.dequeue = function(value) {};
+
+Queue.prototype.size = function(value) {
+  return this._size;
+};
