8a6b55eabe039a5b1de00d37667507a3100dcf86
Hack Reactor Student <student@hackreactor.com>
2017-10-11 20:32:01 -0700

    Instantiate Queue class and add .size method to the queueMethods object (prototypal pattern).

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var instance = Object.create(queueMethods);
+  instance._size = 0;
+  instance._max = 0;
+  instance._min = 0;
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  //enqueue(value)
+  //dequeue
+
+  size: function() {
+    return this._size;
+  }
+};
 
 
