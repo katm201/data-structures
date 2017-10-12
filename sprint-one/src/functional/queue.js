3ff8fe2860e23e59821adf8d65113d714ad6b5c7
Hack Reactor Student <student@hackreactor.com>
2017-10-11 17:22:45 -0700

    Implemented the size method in the Queue function and instantiated the object (functional not shared method pattern)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+    //create minimum value = 0
+    //create max value = 0
+  var _size = 0;
+  var _min = 0;
+  var _max = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    
+    //increase size and mmaximum
+    //assign value to object with maximum as keyt
+
   };
 
   someInstance.dequeue = function() {
+    //store value of removed value in local variable
+    //increase minimum
+    //decrease size
+    //return local value
+    
   };
 
   someInstance.size = function() {
+    return _size;
+
   };
 
   return someInstance;
