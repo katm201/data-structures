c1200333d7d0fc384bdcd5b19783919b0c8f38f2
Hack Reactor Student <student@hackreactor.com>
2017-10-12 16:20:11 -0700

    Completed the graph constructor.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -55,8 +55,9 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-
-
+  for (var key in this.storage) {
+    cb(parseInt(key));
+  }
 };
 
 /*
