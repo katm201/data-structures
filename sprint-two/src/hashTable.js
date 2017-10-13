4624ccb04289d609487961d3d1dfba592735780c
Hack Reactor Student <student@hackreactor.com>
2017-10-13 10:45:49 -0700

    Implemented the insert function of the hash table, but have not tested it yet.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,4 @@
-
+                                                                                                                                                                                                                                                                                                                                
 
 var HashTable = function() {
   this._limit = 8;
@@ -7,6 +7,35 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //check storage array at index to see if it is empty
+  if (this._storage[index]) {
+    var bucket = this._storage[index];
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+      } else {
+        bucket.push([k, v]);
+      }
+    }
+    
+
+   //if it has something
+      //iterate over the bucket
+        //check to see if the key is present in the array
+          //if it is, replace the value
+          //if it isn't, push the touple to the end of the array
+    
+  } else {
+    var bucket = [];
+    this._storage[index] = bucket;
+    bucket.push([k, v]);
+  }
+
+    //if it is empty
+      //create an empty array (new bucket) 
+      //at bucket sub0 add the key value touple
+ 
+
 };
 
 HashTable.prototype.retrieve = function(k) {
