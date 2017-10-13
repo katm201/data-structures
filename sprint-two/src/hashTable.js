bc0d459368f286c9a2a3f8503013ce9ca826c47c
katm201 <katm201@gmail.com>
2017-10-13 11:28:52 -0700

    Implemented remove with our traverse method in the hash table class.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -23,39 +23,35 @@ HashTable.prototype.insert = function(k, v) {
   }
 };
 
-// HashTable.prototype.retrieve = function(k) {
-//   var index = getIndexBelowMaxForKey(k, this._limit);
-//   var bucket = this._storage[index];
-//   if (bucket) {
-//     for (var i = 0; i < bucket.length; i++) {
-//       if (bucket[i][0] === k) {
-//         return bucket[i][1];
-//       }
-//     }
-//   }
-// };
-
 HashTable.prototype.retrieve = function(k) {
-  debugger;
   var result; 
-  this.traverse(k, function(key, bucketElement) {
-    if (bucketElement[0] === key) {
-      result = bucketElement[1];
+  this.traverse(k, function(key, bucket, bucketIndex) {
+    if (bucket[bucketIndex][0] === key) {
+      result = bucket[bucketIndex][1];
     }
   });
   return result;
 };
 
+// HashTable.prototype.remove = function(k) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+//   var bucket = this._storage[index];
+//   if (bucket) {
+//     for (var i = 0; i < bucket.length; i++) {
+//       if (bucket[i][0] === k) {
+//         bucket.splice(i, 1);
+//       }
+//     }
+//   }
+// };
+
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage[index];
-  if (bucket) {
-    for (var i = 0; i < bucket.length; i++) {
-      if (bucket[i][0] === k) {
-        bucket.splice(i, 1);
-      }
+  this.traverse(k, function(key, bucket, bucketIndex) {
+    if (bucket[bucketIndex][0] === key) {
+      bucket.splice(bucketIndex, 1);
     }
-  }
+  });
+
 };
 
 HashTable.prototype.traverse = function(k, func, v) {
@@ -64,7 +60,7 @@ HashTable.prototype.traverse = function(k, func, v) {
   var bucket = this._storage[index];
   if (bucket) {
     for (var i = 0; i < bucket.length; i++) {
-      func(k, bucket[i], v);
+      func(k, bucket, i, v);
     }
   }
 };
