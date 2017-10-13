d0919a42543e87e0d29a301caacdebb30b04c96b
katm201 <katm201@gmail.com>
2017-10-13 11:23:45 -0700

    Implemented a traverse method to DRY our hash table functions.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -23,20 +23,50 @@ HashTable.prototype.insert = function(k, v) {
   }
 };
 
+// HashTable.prototype.retrieve = function(k) {
+//   var index = getIndexBelowMaxForKey(k, this._limit);
+//   var bucket = this._storage[index];
+//   if (bucket) {
+//     for (var i = 0; i < bucket.length; i++) {
+//       if (bucket[i][0] === k) {
+//         return bucket[i][1];
+//       }
+//     }
+//   }
+// };
+
 HashTable.prototype.retrieve = function(k) {
+  debugger;
+  var result; 
+  this.traverse(k, function(key, bucketElement) {
+    if (bucketElement[0] === key) {
+      result = bucketElement[1];
+    }
+  });
+  return result;
+};
+
+HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage[index];
   if (bucket) {
     for (var i = 0; i < bucket.length; i++) {
       if (bucket[i][0] === k) {
-        return bucket[i][1];
+        bucket.splice(i, 1);
       }
     }
   }
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.traverse = function(k, func, v) {
+  func = func || _.identity;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      func(k, bucket[i], v);
+    }
+  }
 };
 
 
