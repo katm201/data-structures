2c2c05605fb0df08a2f70b84be388624557e4abd
katm201 <katm201@gmail.com>
2017-10-13 20:50:28 -0700

    Added the head and tail methods for the doubly linked list class.

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -1,3 +1,68 @@
 var DoublyLinkedList = function() {
+  this.head = null;
+  this.tail = null;
+};
+
+DoublyLinkedList.prototype.addToTail = function(value) {
+  var newNode = Node(value);
+  if (this.head === null) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    let oldTail = this.tail;
+    this.tail = newNode;
+    oldTail.next = newNode;
+    this.tail.previous = oldTail;
+  }
+};
+
+DoublyLinkedList.prototype.addToHead = function(value) {
+  var newNode = Node(value);
+  if (this.tail === null) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    let oldHead = this.head;
+    this.head = newNode;
+    this.head.next = oldHead;
+    oldHead.previous = this.head;
+  }
+};
+
+DoublyLinkedList.prototype.removeTail = function() {
+  if (this.head === this.tail) {
+    let nodeValue = this.head.value;
+    this.head = null;
+    this.tail = null;
+    return nodeValue;
+  }
+  if (this.tail) {
+    let oldTail = this.tail;
+    this.tail = oldTail.previous;
+    this.tail.next = null;
+    oldTail.previous = null;
+    return oldTail.value;
+  }
+};
+
+DoublyLinkedList.prototype.removeHead = function() {
+  if (this.head === this.tail) {
+    let nodeValue = this.head.value;
+    this.head = null;
+    this.tail = null;
+    return nodeValue;
+  }
+  if (this.head) {
+    let oldHead = this.head;
+    this.head = oldHead.next;
+    this.head.previous = null;
+    oldHead.next = null;
+    return oldHead.value;
+  }
+};
+
+DoublyLinkedList.prototype.contains = function(target) {
 
 };
+
+
